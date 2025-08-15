9384afc60be3870689ea4dc0f66f7253f199f41a
Enzo <enzio@garryhost.com>
2025-08-15 12:40:59 +0200

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -4,11 +4,26 @@ return [
 
     'default' => env('LOG_CHANNEL', 'daily'),
 
-    'lua' => [
-        'driver' => 'daily',
-        'path' => storage_path('logs/lua.log'),
-        'level' => env('LOG_LEVEL', 'debug'),
-        'days' => 14,
+    'channels' => [
+        'daily' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+        ],
+
+        'lua' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/lua.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+        ],
+
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['daily'],
+            'ignore_exceptions' => false,
+        ],
     ],
 
 ];
