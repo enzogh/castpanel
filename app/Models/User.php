e866579245b1fc968bbb7b1775222a77089794be
Enzo <enzio@garryhost.com>
2025-08-17 06:43:51 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -249,6 +249,11 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         return $this->hasMany(UserSSHKey::class);
     }
 
+    public function tickets(): HasMany
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
     /**
      * Returns all the activity logs where this user is the subject — not to
      * be confused by activity logs where this user is the _actor_.
